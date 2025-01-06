8d8dc33cb77c0c09df6e27a915398808532f2ddd
Markus Kirberg <markus@flobby.net>
2025-01-06 21:00:53 +0000

    esp idf new

--- src/esp32/esp32_adc.c
+++ src/esp32/esp32_adc.c
@@ -19,8 +19,10 @@
 
 #include <stdbool.h>
 
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "esp_adc/adc_oneshot.h"
+#include "hal/adc_types.h"
 
 #include "common/platform.h"
 
@@ -28,24 +30,26 @@
 #include "mgos_sys_config.h"
 
 static int s_vref = 1100;
-static int s_width = ADC_WIDTH_12Bit;
+static int s_width = ADC_BITWIDTH_12;
 
 struct esp32_adc_channel_info {
   int pin;
-  adc1_channel_t ch;
+  adc_channel_t ch;
   adc_atten_t atten;
-  esp_adc_cal_characteristics_t chars;
+
+  adc_oneshot_unit_handle_t handle;
+  adc_cali_handle_t cali_h;
 };
 
 static struct esp32_adc_channel_info s_chans[8] = {
-    {.pin = 36, .ch = ADC1_CHANNEL_0, .atten = ADC_ATTEN_DB_11},
-    {.pin = 37, .ch = ADC1_CHANNEL_1, .atten = ADC_ATTEN_DB_11},
-    {.pin = 38, .ch = ADC1_CHANNEL_2, .atten = ADC_ATTEN_DB_11},
-    {.pin = 39, .ch = ADC1_CHANNEL_3, .atten = ADC_ATTEN_DB_11},
-    {.pin = 32, .ch = ADC1_CHANNEL_4, .atten = ADC_ATTEN_DB_11},
-    {.pin = 33, .ch = ADC1_CHANNEL_5, .atten = ADC_ATTEN_DB_11},
-    {.pin = 34, .ch = ADC1_CHANNEL_6, .atten = ADC_ATTEN_DB_11},
-    {.pin = 35, .ch = ADC1_CHANNEL_7, .atten = ADC_ATTEN_DB_11},
+    {.pin = 36, .ch = ADC_CHANNEL_0, .atten = ADC_ATTEN_DB_12},
+    {.pin = 37, .ch = ADC_CHANNEL_1, .atten = ADC_ATTEN_DB_12},
+    {.pin = 38, .ch = ADC_CHANNEL_2, .atten = ADC_ATTEN_DB_12},
+    {.pin = 39, .ch = ADC_CHANNEL_3, .atten = ADC_ATTEN_DB_12},
+    {.pin = 32, .ch = ADC_CHANNEL_4, .atten = ADC_ATTEN_DB_12},
+    {.pin = 33, .ch = ADC_CHANNEL_5, .atten = ADC_ATTEN_DB_12},
+    {.pin = 34, .ch = ADC_CHANNEL_6, .atten = ADC_ATTEN_DB_12},
+    {.pin = 35, .ch = ADC_CHANNEL_7, .atten = ADC_ATTEN_DB_12},
 };
 
 static struct esp32_adc_channel_info *esp32_adc_get_channel_info(int pin) {
@@ -56,11 +60,28 @@ static struct esp32_adc_channel_info *esp32_adc_get_channel_info(int pin) {
 }
 
 static bool esp32_update_channel_settings(struct esp32_adc_channel_info *ci) {
-  if (adc1_config_channel_atten(ci->ch, ci->atten) != ESP_OK) {
+  adc_oneshot_unit_init_cfg_t uconfig = {.unit_id = ADC_UNIT_1};
+
+  // TODO: leaking handle, need to call adc_oneshot_del_unit
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&uconfig, &ci->handle));
+
+  adc_oneshot_chan_cfg_t config = {
+      .bitwidth = s_width,
+      .atten = ci->atten,
+  };
+  if (adc_oneshot_config_channel(ci->handle, ci->ch, &config) != ESP_OK) {
     return false;
   }
 
-  esp_adc_cal_characterize(ADC_UNIT_1, ci->atten, s_width, s_vref, &ci->chars);
+  adc_cali_line_fitting_config_t sconfig;
+  sconfig.unit_id = ADC_UNIT_1;
+  // sconfig.chan = ci->ch;
+  sconfig.atten = ci->atten;
+  sconfig.bitwidth = s_width;
+  // sconfig->default_vref = s_vref;
+
+  adc_cali_create_scheme_line_fitting(&sconfig, &ci->cali_h);
+
   return true;
 }
 
@@ -82,14 +103,19 @@ bool mgos_adc_enable(int pin) {
 int mgos_adc_read(int pin) {
   struct esp32_adc_channel_info *ci = esp32_adc_get_channel_info(pin);
   if (ci == NULL) return false;
-  return adc1_get_raw(ci->ch);
+  int raw = 0;
+  ESP_ERROR_CHECK(adc_oneshot_read(ci->handle, ci->ch, &raw));
+  return raw;
 }
 
 int mgos_adc_read_voltage(int pin) {
   struct esp32_adc_channel_info *ci = esp32_adc_get_channel_info(pin);
   if (ci == NULL) return false;
-  uint32_t voltage;
-  esp_adc_cal_get_voltage(ci->ch, &ci->chars, &voltage);
+  int voltage;
+  int raw;
+  ESP_ERROR_CHECK(adc_oneshot_read(ci->handle, ci->ch, &raw));
+  adc_cali_raw_to_voltage(ci->cali_h, raw, &voltage);
+
   return voltage;
 }
 
@@ -98,10 +124,10 @@ void esp32_adc_set_vref(int vref_mv) {
 }
 
 void esp32_adc_set_width(int width) {
-  if ((width >= ADC_WIDTH_BIT_9) && (width <= ADC_WIDTH_BIT_12)) {
+  if ((width >= 0) && (width <= ADC_BITWIDTH_13)) {
     s_width = width;
   }
-  adc1_config_width(s_width);
+  // TODO: update all channels?
 }
 
 bool mgos_adc_init(void) {
